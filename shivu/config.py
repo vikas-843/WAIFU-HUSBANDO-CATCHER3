0e930fbf9564b29fa0de64066a174e3a2852a4e0
VG-TEAM <152384469+VG-TEAM@users.noreply.github.com>
2024-01-13 21:11:43 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,11 +2,11 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
+    OWNER_ID = "5932230962"
     sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    GROUP_ID = -1001875834087
+    TOKEN = "6735069372:AAGr0jkAi2WI-RYfLsI3snKTGtPcTRQnR8Q"
+    mongo_url = "mongodb+srv://vikas:vikas@vikas.yfezexk.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
