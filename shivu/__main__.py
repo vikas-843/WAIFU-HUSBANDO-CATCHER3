c2ec059964613d69b86215506b0c8278a3f15adf
VG-TEAM <152384469+VG-TEAM@users.noreply.github.com>
2024-01-21 18:29:54 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -47,9 +47,9 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
         
         chat_frequency = await user_totals_collection.find_one({'chat_id': chat_id})
         if chat_frequency:
-            message_frequency = chat_frequency.get('message_frequency', 1)
+            message_frequency = chat_frequency.get('message_frequency', 50)
         else:
-            message_frequency = 1
+            message_frequency = 50
 
         
         if chat_id in last_user and last_user[chat_id]['user_id'] == user_id:
